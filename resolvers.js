ccaadeea95e2f6cdd65dd0b734bb940de6a9828e
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2022-12-30 10:29:44 +0700

    first commit

---
+++ resolvers.js
@@ -0,0 +1,17 @@
+import { Company, Job } from './db.js';
+
+export const resolvers = {
+  Query: {
+    job: (_root, { id }) => Job.findById(id),
+    jobs: async () => Job.findAll(),
+    company: (_root, { id }) => Company.findById(id),
+  },
+
+  Job: {
+    company: (job) => Company.findById(job.companyId),
+  },
+
+  Company: {
+    jobs: (company) => Job.findAll((job) => job.companyId === company.id),
+  },
+};
